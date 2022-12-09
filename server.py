135b9a38e1af466352502bddaca0dc9266f897c9
chl0e3e <foxleychloe@gmail.com>
2022-12-09 04:42:19 +0000

    Fix presence, put the code in but my attention span

--- server.py
+++ server.py
@@ -267,7 +267,6 @@ class NewsWall:
 
             return aggregation
 
-        aggregation.append({"$project": {"presence": "0"}})
 
         await send({"cmd": "log", "data": self.logs_buffer})
 
@@ -290,6 +289,7 @@ class NewsWall:
                             aggregation = await build_aggregation(data["data"], pagination, feed_cursor)
                             aggregation.append({ "$sort": { "_id" : -1} })
                             aggregation.append({ "$limit": 100 })
+                            aggregation.append({"$project": {"presence": "0"}})
                             docs = []
                             async for doc in self.async_mongodb_database["empty"].aggregate(aggregation):
                                 docs.append(doc)
